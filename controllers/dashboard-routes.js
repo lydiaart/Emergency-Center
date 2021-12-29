2c516e88f92c27704651adc9ad9863f640a3e8b9
mogannam <JoeMogannam@gmail.com>
2021-12-28 16:16:17 -0800

    added front end and backend code to send text message out whenever a user posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,31 +1,102 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
-const withAuth = require('../utils/auth');
 
-// // get all posts for dashboard
-// router.get('/', withAuth, (req, res) => {
- 
-//     res.render('dashboard', { loggedIn: true });
-   
-// });
+const { Post, User, Comment, Vote } = require('../models');
+const axios = require('axios')
+const withAuth = require('../utils/auth');
+require('dotenv').config();
+const googleApiKey = process.env.googleApiKey
+
+//twillio api configs
+const twillioAccountSid = process.env.twillioAccountSid;
+const TwillioAuthToken = process.env.TwillioAuthToken;
+const twillioFromNumber = process.env.twillioFromNumber;
+const clientTwillio = require('twilio')(twillioAccountSid, TwillioAuthToken);
+
+
+//https://maps.googleapis.com/maps/api/geocode/json?latlng=38.4097,-122.7388&key=AIzaSyA0xTQIZzOUGSAytuCHRd98nNSzuinHxKE
 
 
 
 router.post('/posts', withAuth, (req, res) => {
   // expects {title: 'Flooding in Arden Fair mall', contents: 'South side of mall', 
   //          latitude: -143, longitude: 45, user_id: 1 }
-  console.log(`\n\n\n\n \t\t\t\t ******* in dashboard-routes.js:/posts`)
+  //console.log(`\n\n\n\n \t\t\t\t ******* in dashboard-routes.js:/posts`)
+  const severity =  req.body.severity
+  const title = req.body.title
+  const contents = req.body.contents
+  const latitude = req.body.latitude
+  const longitude = req.body.longitude
+  let formatted_address;
+  let usersaToAlert;
+
   Post.create({
-          title: req.body.title,
-          contents: req.body.contents,
-          latitude: req.body.latitude,
-          longitude: req.body.longitude,
-          user_id: req.session.user_id
+          title:  title,
+          contents: contents,
+          latitude: latitude,
+          longitude: longitude ,
+          user_id: req.session.user_id,
+          severity : severity
       })
       .then(dbPostData => {
-        //console.log(JSON.stringify(dbPostData))
+        //console.log(`\n\n\n\n \t\t\t\t\t ********* in dashboard-routes.js:posts severity is : ${JSON.stringify(severity)}`)
         res.json(dbPostData)
+      })
+      .then(() => {
+        //console.log(`\n\n\n\n \t\t\t\t\t  in dashboard-routes.js:posts severity is : ${JSON.stringify(severity)}`)
+        console.log("\n\n\n\n\n \t\t\t\t\t ****** dashboard-routes.js: before  testing twillio")
+        if(severity === "high" ||severity === "life-threatening"  ){
+          const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleApiKey}`
+
+          console.log("\n\n\n\n\n \t\t\t\t\t ****** dashboard-routes.js: testing twillio")
+          console.log(`url is ${url}`)
+
+            // 1) pass lat & lon to  google api to get county
+            axios({
+              method:'get',
+              url : url,
+            })
+            .then(function (responseMapAPI) {
+              console.log(`\n\n\n\n\n\n \t\t\t\t\t\t ************ axios call made`)
+              const MapAPiData = responseMapAPI.data 
+              console.log(MapAPiData.results[7].formatted_address)
+              formatted_address = MapAPiData.results[7].formatted_address
+              //res.send(JSON.stringify(MapAPiData));
+            }).then(() => {
+               // 2) get list of users & phone numbers and the corelating county thet subscibe to
+ 
+               if(formatted_address){
+                
+                let usersArr = fakeJSONUsers
+                usersaToAlert = usersArr.filter(user => user.formatted_address === formatted_address);
+                console.log(`usersaToAlert: ${JSON.stringify(usersaToAlert)}`)
+
+
+               }
+              
+
+            }).then(()=>{
+              // 3) call twillio
+              if(usersaToAlert){
+                usersaToAlert.forEach(user => {
+
+                  clientTwillio.messages
+                    .create({
+                        body: `${title} (${severity})| ${contents} \n ${latitude} ${longitude}`,
+                        from: twillioFromNumber,
+                        to: user.phone,
+                      })
+                  
+                })
+
+              }
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+          
+          
+        }
       })
       .catch(err => {
           console.log(err);
--- public/assets/js/dashboard-posts.js
+++ public/assets/js/dashboard-posts.js
@@ -12,19 +12,24 @@ async function updateGeoCoords(position) {
     localStorage.setItem("latitude", latitude );
     localStorage.setItem("longitude", longitude );
   }
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
 
 async function submitPost(event) {
     event.preventDefault();
     console.log(" \n\n\n\n\n\n \t\t\t\t\t ******* in dashboard-posts.js:submitPosts")
     const title = document.querySelector('#report-title').value.trim();
     const contents = document.querySelector('#report-desc').value.trim();
-    console.log("\n\n\n\n\n\n \t\t\t\t\t ********* in dashboard-posts.js:submitPosts after query selector")
+    const severity = document.querySelector('#severity').value
+    console.log(`\n\n\n\n\n\n \t\t\t\t\t ********* in dashboard-posts.js:submitPosts after query selector, severity ${JSON.stringify(severity)}`)
 
-    updateGeoCoords();
+   
+    //await navigator.geolocation.getCurrentPosition(updateGeoCoords, error);
     const latitude = localStorage.getItem("latitude");
     const longitude = localStorage.getItem("longitude")
     console.log("in dashboard-posts.js:submitPosts before if statement")
-    const user_id = 3;
+    //const user_id = 3;
     if (title && contents && latitude && longitude)  {
         console.log("in dashboard-posts.js:submitPosts entered if statement")
       const response = await fetch('/dashboard/posts', {
@@ -33,8 +38,9 @@ async function submitPost(event) {
           title,
           contents,
           latitude,
-          longitude, 
-          user_id
+          longitude,
+          severity 
+          //user_id
         }),
         headers: { 'Content-Type': 'application/json' }
       });
@@ -47,6 +53,12 @@ async function submitPost(event) {
       } else {
         alert(response.statusText);
       }
+
+      //--------------
+
     }
+
+   
+
   }
   document.querySelector('.post-form').addEventListener('submit', submitPost);
