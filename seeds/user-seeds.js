6cdba9ee61137c20f13ded89c05ebbc4f73c0bf4
Lydia Loy <lydia_art@yahoo.com>
2021-12-30 14:38:53 -0600

    added firstName and lastName into user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -6,43 +6,63 @@ const {
 
 const userdata = [{
         username: 'david',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'david',
+        lastName: 'barn'
     },
     {
         username: 'nick',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'nick',
+        lastName: 'las'
     },
     {
         username: 'stephen',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'stephen',
+        lastName: 'job'
     },
     {
         username: 'rosanne',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'rosanne',
+        lastName: 'han'
     },
     {
         username: 'marvin',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'marvin',
+        lastName: 'bruce'
     },
     {
         username: 'william',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'william',
+        lastName: 'geroge'
     },
     {
         username: 'henry',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'henry',
+        lastName: 'brown'
     },
     {
         username: 'cindy',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'cidy',
+        lastName: 'ron'
     },
     {
         username: 'blake',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'blake',
+        lastName: 'water'
     },
     {
         username: 'roger',
-        password: 'password123'
+        password: 'password123',
+        firstName: 'roger',
+        lastName: 'ken'
     }
 ];
 
