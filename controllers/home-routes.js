4f5d5f00c8a058d33818ee6ecff290d277579a02
Lydia Loy <lydia_art@yahoo.com>
2021-12-30 18:57:07 -0600

    change log-in from email to username

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,29 +3,64 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
 router.get('/', (req, res) => {
-    console.log('======================');
-    //console.log(req.session);
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
-      });
+  console.log('======================');
+  //console.log(req.session);
+  res.render('homepage', {
+    loggedIn: req.session.loggedIn
   });
+});
 
 
 router.get('/login', (req, res) => {
-    //console.log(`in home-routes.js:/login`)
-  if (req.session.loggedIn) {
-    console.log(`\n\n\n\n\ -------in home-routes.js:/login entered if`)
-    // res.render('login', {
-    //     loggedIn: req.session.loggedIn
-    //   });
-      res.render('report-form', {loggedIn: req.session.loggedIn})
-    //res.redirect('/')
-
-    
-    return;
-  }
-  
-  res.render('login');
+  res.render('login')
 });
+
+ 
+router.get('/signup', (req, res) => {
+  res.render('signup')
+});
+
+router.get('/comments', (req, res) => {
+  res.render('comments')
+});
+
+router.get('/view-posts', (req, res) => {
+  Post.findAll({
+    include: [
+      {
+        model: Comment,
+        include: [
+          {
+            model: User,
+          }
+        ]
+      }, {
+        model: User,
+      }
+    ]
+  })
+    .then(postData => {
+      const posts = postData.map(post => {
+        return post.get({
+          plain: true
+        })
+      })
+
+      console.log(posts);
+
+      res.render('view-posts', { posts })
+    })
+
+
+});
+
+router.get('/create-post', (req, res) => {
+  res.render('create-post')
+});
+
+router.get('/about', (req, res) => {
+  res.render('about')
+})
+
 
 module.exports = router;
