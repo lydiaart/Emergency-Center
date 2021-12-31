c071d5cb276cc2e4b5f72fa8d8bb14e6069c3975
robyng <robyngraham01@gmail.com>
2021-12-31 12:58:33 -0800

    comment out name field, last name field, avatar field in sign up

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,19 +1,21 @@
-var dropdown = document.querySelector(".dropdown");
-var imagePlaceHolder = document.querySelector("#image-placeholder");
-var nameplaceHolder = document.querySelector("#name-placeholder");
-var firstName = document.querySelector("#first-name");
-var lastName = document.querySelector("#last-name")
+//var dropdown = document.querySelector(".dropdown");
+//var imagePlaceHolder = document.querySelector("#image-placeholder");
+//var nameplaceHolder = document.querySelector("#name-placeholder");
+//var firstName = document.querySelector("#first-name");
+//var lastName = document.querySelector("#last-name")
 
-function changeAvatar(event){
-event.preventDefault()
+// function changeAvatar(event){
+// event.preventDefault()
 
-console.log(this.value)
-imagePlaceHolder.setAttribute("src",this.value)
-nameplaceHolder.textContent = firstName.value + " " + lastName.value
+// console.log(this.value)
+// imagePlaceHolder.setAttribute("src",this.value)
+// nameplaceHolder.textContent = firstName.value + " " + lastName.value
 
-}
+// }
 
-dropdown.addEventListener("change", changeAvatar)
+// dropdown.addEventListener("change", changeAvatar)
+
+///////////////////////////////////////////////////////////////////////////////////
 
 // code below is from joe's branch feature/signin-signup with working signup form
 
@@ -52,27 +54,27 @@ async function signupFormHandler(event) {
     }
   }
 
-  async function loginFormHandler(event) {
-    event.preventDefault();
+  // async function loginFormHandler(event) {
+  //   event.preventDefault();
   
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  //   const username = document.querySelector('#username-login').value.trim();
+  //   const password = document.querySelector('#password-login').value.trim();
   
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
+  //   if (username && password) {
+  //     const response = await fetch('/api/users/login', {
+  //       method: 'post',
+  //       body: JSON.stringify({
+  //         username,
+  //         password
+  //       }),
+  //       headers: { 'Content-Type': 'application/json' }
+  //     });
   
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+  //     if (response.ok) {
+  //       document.location.replace('/dashboard');
+  //     } else {
+  //       alert(response.statusText);
+  //     }
+  //   }
+  // }
   
