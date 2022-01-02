857a7c74ee1075d66d618521a4ced7c69c9f41f3
David Barron <90076120+webgeekbear@users.noreply.github.com>
2022-01-01 19:02:43 -0800

    Code cleanup

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
 module.exports = {
-    showyear: ()=>{
+    showyear: () => {
         var CurrentYear = new Date().getFullYear()
         return CurrentYear
-    } ,
+    },
     format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
             date
